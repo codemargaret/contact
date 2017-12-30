9d5709e3daae48426d1c2bab301a81b94fcc647f
Margaret Berry <codeberry1@gmail.com>
2017-12-30 13:02:02 -0800

    remove commented code

--- lib/contact.rb
+++ lib/contact.rb
@@ -3,20 +3,6 @@ class Contact
   attr_accessor :first_name, :last_name, :address, :phone_number
   attr_reader :id
 
-  # def first_name  reader attr
-  #   @first_name
-  # end
-
-  # def first_name(first_name)  writer attr
-  #   @first_name = first_name
-  # end
-
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
-  #   @id = @@list.length + 1
-  # end
-
   def initialize(attributes)
     @first_name = attributes.fetch("first_name")
     @last_name = attributes.fetch("last_name")
@@ -26,18 +12,11 @@ class Contact
     @id = @@list.length + 1
   end
 
-  def self.all #this refers to all the items in the @@list
+  def self.all
     @@list
   end
 
-  # def self.find()
-  #   @@list.all? do |contact|
-  #    contact.first_name == contact.self
-  #       return contact
-  #   end
-  # end
-
-  def self.find(id) #class methods
+  def self.find(id)
     contact_id = id.to_i()
     @@list.each do |contact|
       if contact.id == contact_id
@@ -47,7 +26,6 @@ class Contact
   end
 
   def save
-    # @@list.each do |contact|
       @@list.push(self)
   end
 
--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -3,34 +3,21 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe("Contact") do
-#   before() do
-#       Contact.clear()
-#     end
-
-  describe('my contacts', {:type => :feature}) do
-    it ('processes the user entry and returns a list of items') do
-      visit('/')
-      fill_in('first_name', :with => 'margaret')
-      fill_in('last_name', :with => 'kitten')
-      click_button('add contact')
-      expect(page).to have_content('margaret')
-    end
+describe('my contacts', {:type => :feature}) do
+  it ('processes the user entry and returns a list of items') do
+    visit('/')
+    fill_in('first_name', :with => 'margaret')
+    fill_in('last_name', :with => 'kitten')
+    click_button('add contact')
+    expect(page).to have_content('margaret')
   end
+end
 
-  # describe('a contact', {:type => :feature}) do
-  #   it ('lists the first name') do
-  #     visit('/contact/1')
-  #     expect(page).to have_content('margaret kitten')
-  #   end
-  # end
-
-  describe('a contact', {:type => :feature}) do
-    it ('goes to the detail page') do
-      visit('/')
-      save_and_open_page
-      click_link("kitten margaret")
-      expect(page).to have_content('margaret')
-    end
+describe('a contact', {:type => :feature}) do
+  it ('goes to the detail page') do
+    visit('/')
+    save_and_open_page
+    click_link("kitten margaret")
+    expect(page).to have_content('margaret')
   end
-# end
+end
